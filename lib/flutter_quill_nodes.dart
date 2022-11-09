36c1d3ecd6a707123a8701b11bc60846710309a9
huanhua.li <616436641@qq.com>
2022-11-09 11:32:18 +0800

    Revert "fix: 还原flutter_quill.dart文件，添加flutter_quill_nodes.dart文件"
    This reverts commit 19bf3167ddf61abca8220b1bd1a2cedf2f689662.

--- lib/flutter_quill_nodes.dart
+++
@@ -1,8 +0,0 @@
-library flutter_quill;
-
-export 'src/models/documents/nodes/line.dart';
-export 'src/models/documents/nodes/block.dart';
-export 'src/models/documents/nodes/node.dart';
-export 'src/models/documents/nodes/container.dart';
-
-export 'src/utils/color.dart';
